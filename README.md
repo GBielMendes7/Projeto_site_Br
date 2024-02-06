c6d0f4907eb5107b1dbef9fa9119e65f7c8b375d
GBielMendes7 <99516155+GBielMendes7@users.noreply.github.com>
2024-02-06 01:52:04 -0300

    react

--- README.md
+++
@@ -1,2 +0,0 @@
-# Projeto_site_Br
- 
--- Site_Br/assets/Javascript/scroll-home.js
+++
@@ -1,7 +0,0 @@
-window.sr = ScrollReveal({ reset: true });
-
-sr.reveal(".escudos",{ duration: 1000});
-
-sr.reveal(".conteiner1",{ duration: 1000});
-
-sr.reveal(".col2",{ duration: 1000});
